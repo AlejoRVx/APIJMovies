24a1e80efca7bddad57ca571cac6f6c1208da6d1
AlejoRVx <alejandroramirezvela13@gmail.com>
2025-10-29 21:15:28 -0500

    Agregar archivos de proyecto.

---
+++ DAL/Models/AuditBase.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.J.Movies.DAL.Models
+{
+    public class AuditBase
+    {
+        //Decorator a Data Annotations
+        [Key] //PK
+        public virtual int Id { get; set; } //PK de todas las tablas
+
+        public virtual DateTime CreatedDate { get; set; } //este me sirve para guardar la fecha de creación de un registro de base de datos
+
+        public virtual DateTime ModifiedDate { get; set; } //para guardar la fecha de modificación de un registro de base de datos
+    }
+}
---
+++ DAL/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.J.Movies.DAL.Models
+{
+    public class Category : AuditBase
+    {
+        //propiedades!! se convierten en columnas en la base de datos
+        //comando prop para generar una propiedad
+        [Required] //este decorador indica que el campo es obligatorio (no acepta nulls)
+        public string Name { get; set; }
+    }
+}

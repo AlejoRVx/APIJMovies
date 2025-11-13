36d3aa4af6ac38446c6e6f24f41ec9b7f9b25ebb
AlejoRVx <alejandroramirezvela13@gmail.com>
2025-11-12 21:13:46 -0500

    AutoMapper and DTOs created

---
+++ MoviesMapper/Mappers.cs
@@ -0,0 +1,16 @@
+﻿using API.J.Movies.DAL.Dtos;
+using API.J.Movies.DAL.Models;
+using AutoMapper;
+
+namespace API.J.Movies.MoviesMapper
+{
+    public class Mappers: Profile
+    {
+        public Mappers()
+        {
+            //Category Mappings
+            CreateMap<Category, CategoryDto>().ReverseMap();
+            CreateMap<Category, CategoryCreateDto>().ReverseMap();
+        }
+    }
+}
